152b66ec76d44106d44b2b7df75d2b3cd8d86beb
user <amba9583@colorado.edu>
2015-11-02 15:09:52 -0700

    Added web home page, contains code for database testing

---
+++ rj_home.php
@@ -0,0 +1,39 @@
+<!doctype html>
+	<html lang="en">
+	<head>
+		<link rel="shortcut icon" href="buff.jpg" type="image/jpg"/>
+		<link rel="stylesheet" href="rj_style.css" type="text/css"/>
+		<link href=’http://fonts.googleapis.com/css?family=Droid+Sans’ rel=’stylesheet’ type=’text/css’>
+		<title> Home </title>
+	</head>
+	<body>
+		<header>
+			<center><h1>CU: Ralphies List</h1></center>
+		</header>
+		<input type="text" class="advancedSearchTextBox" />
+		<nav>
+			<p> <b>These are my favorite links:</b>
+			<ul>
+				<li><a href="rj_home.html">Home</a></li>
+				<li><a href="rj_about.html">About</a></li>
+				<li><a href="rj_gallery.html">Gallery</a></li>
+				<li><a href="http://www.computerhope.com">Computer Hope</a></li>
+				<li><a href="http://www.google.com">Google</a></li>
+			</ul>
+			</p>
+		</nav>
+		<section>
+			<info>
+				<p> <b>CU ralphies junk </b><br>
+					TEST
+				</p>
+				<p>
+						<?php
+							echo("<b>This is a test</b>");
+						?>
+				</p>
+			</info>
+			
+		</section>
+	</body>
+	</html>
