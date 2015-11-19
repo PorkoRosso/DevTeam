4bb3f586579e39c7bed8780b61bb0cf350991b5a
user <amba9583@colorado.edu>
2015-11-18 17:46:40 -0700

    Added php file for login

---
+++ login.php
@@ -0,0 +1,37 @@
+<!doctype html>
+	<html lang="en">
+	<head>
+		<link rel="shortcut icon" href="buff.jpg" type="image/jpg"/>
+		<link rel="stylesheet" href="rj_style.css" type="text/css"/>
+		<link href=’http://fonts.googleapis.com/css?family=Droid+Sans’ rel=’stylesheet’ type=’text/css’>
+		<title> Login </title>
+	</head>
+	<body>
+		<header>
+			<center><h1>CU: Ralphies List</h1></center>
+		</header>
+		<nav>
+			<ul>
+				<li><a href="rj_user.html">Home</a></li>
+				<li><a href="rj_browse.html">Browse</a></li>
+				<li><a href="rj_login.html">Login</a></li>
+				<li><a href="http://www.google.com">Google</a></li>
+			</ul>
+			</p>
+		</nav>
+		<section>
+			<info>
+			<form action="action_page.php" style="border: 3px solid #101417; background-color: #f1f1c1; padding: 25px 70px 25px 70px;" 	 >
+				Username:<br>
+				<input type="text" name="user">
+				<br>
+				Password:<br>
+				<input type="password" name="pass">
+				<br><br>
+				<input type="submit" value="Submit">
+				<input type="submit" value="Sign Up">
+			</form>
+			</info>	
+		</section>
+	</body>
+	</html>
