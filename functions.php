b64bc03abbccc87a1d8be9d13090e2c716c67daa
ABahirat <amba9583@colorado.edu>
2015-11-30 13:34:07 -0700

    Login function testing

--- functions.php
+++ functions.php
@@ -125,6 +125,11 @@ function Checklogin(){
 		echo "Failed to connect to Database</br>";
 	}
 
+	$email = SanitizeForSql($email);
+	$email = mysql_real_escape_string($email);
+	$password = SanitizeForSql($password);
+	$password = mysql_real_escape_string($password);
+ 
 	$query = mysql_query("Select * FROM Users WHERE user_email = '$email'");
 
 	$numRows = mysql_num_rows($query);
