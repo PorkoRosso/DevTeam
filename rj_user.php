bed83bc12c48f9d02211e59428b24080c79b1ed7
ABahirat <amba9583@colorado.edu>
2015-11-30 14:15:52 -0700

    Changed user html page to php

---
+++ rj_user.php
@@ -0,0 +1,60 @@
+<!doctype html>
+	<html lang="en">
+	<head>
+		<link rel="shortcut icon" href="buff.jpg" type="image/jpg"/>
+		<link rel="stylesheet" href="rj_style.css" type="text/css"/>
+		<link href=’http://fonts.googleapis.com/css?family=Droid+Sans’ rel=’stylesheet’ type=’text/css’>
+		<title> User </title>
+	</head>
+	<body>
+		<header>
+			<center><h1>CU: Ralphies List</h1></center>
+		</header>
+		<input type="text" class="advancedSearchTextBox" />
+		<nav>
+			<ul>
+				<li><a href="rj_user.html">Home</a></li>
+				<li><a href="rj_browse.html">Browse</a></li>
+				<li><a href="rj_login.html">Login</a></li>
+				<li><a href="http://www.google.com">Google</a></li>
+				<li><a href="add_item.html">New Post</a></li>
+			</ul>
+			</p>
+		</nav>
+		<section>
+			<img class = "userpic" src = "pr.jpg" alt = "baahhh">
+			<userinfo>
+				Paul Revere
+				<br>Junior
+				<br>CSCI major
+			</userinfo>
+		<info>
+			
+			
+			<table id="t01">
+				<tr>
+					<th>Date Posted</th>
+					<th>Item</th>
+					<th>Trade/Sale</th> 
+					<th>Picture</th>
+					
+				</tr>
+				<tr>
+					<td>11/10/2015</td>
+					<td>dill pickle</td> 
+					<td>sale</td>
+					<td><img src="pickle.jpg" style="width:auto; height:50%;"></td>
+				
+				</tr>
+				<tr>
+					<td>11/20/2015</td>
+					<td>wolverine</td> 
+					<td>trade</td>
+					<td><img src="wolv.jpg" style="width:auto; height:50%;"></td>
+				
+				</tr>
+			</table>
+		</info>
+		</section>
+	</body>
+	</html>
