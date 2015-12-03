2d6e7e4ffaec7c703c33fd599541be17af973fd0
ABahirat <amba9583@colorado.edu>
2015-12-02 19:19:03 -0700

    Fix tab issue on upload item

--- functions.php
+++ functions.php
@@ -210,33 +210,34 @@ function upload_item(){
 		$item = trim($_POST['Item_name']);
 		$item = mysql_real_escape_string($item);
 
-	}
-	// Check if file already exists
-	/**if (file_exists($target_file)) {
-		echo "Sorry, file already exists."; //Don't need this in final implimentation
-		$uploadOk = 0;
-	}**/
-	// Check file size
-	if ($_FILES["fileToUpload"]["size"] > 500000) {
-		echo "Sorry, your file is too large.";
-		$uploadOk = 0;
-	}
-	// Allow certain file formats
-	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-	&& $imageFileType != "gif" ) {
-		echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-		$uploadOk = 0;
-	}
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-		echo "Sorry, your file was not uploaded.";
-	// if everything is ok, try to upload file
-	} else {
-		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-			//This is where query will go
+	
+		// Check if file already exists
+		/**if (file_exists($target_file)) {
+			echo "Sorry, file already exists."; //Don't need this in final implimentation
+			$uploadOk = 0;
+		}**/
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 500000) {
+			echo "Sorry, your file is too large.";
+			$uploadOk = 0;
+		}
+		// Allow certain file formats
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+		&& $imageFileType != "gif" ) {
+			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+			$uploadOk = 0;
+		}
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+			echo "Sorry, your file was not uploaded.";
+		// if everything is ok, try to upload file
 		} else {
-			echo "Sorry, there was an error uploading your file.";
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+				//This is where query will go
+			} else {
+				echo "Sorry, there was an error uploading your file.";
+			}
 		}
 	}	
 }
