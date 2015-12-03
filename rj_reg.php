69f119f2a229dac84c4f25dae369c80b586c3f8e
dbostany <dabo9011@colorado.edu>
2015-12-03 08:35:23 -0700

    reg and form styling

--- rj_reg.php
+++ rj_reg.php
@@ -21,18 +21,18 @@
 		<section>
 			<info>
 
-			<form method="post" style="border: 3px solid #101417; background-color: #cfb87b; padding: 25px 70px 25px 70px;">
-			First Name: <input type="text" name="user_first_name">
+			<form method="post" style="border: 3px solid #101417;  background-color: #cfb87b; padding: 25px 70px 25px 30px">
+			First Name: <br> <input type="text" name="user_first_name">
 			<br><br>
-			Last Name: <input type="text" name="user_last_name">
+			Last Name:<br> <input type="text" name="user_last_name">
 			<br><br>
-			Phone Number: <input type="text" name="user_phone">
+			Phone Number:<br> <input type="text" name="user_phone">
 			<br><br>
-			E-mail: <input type="text" name="user_email">
+			E-mail:<br> <input type="text" name="user_email">
 			<br><br>
-			Password: <input type="password" name="user_pass">
+			Password:<br> <input type="password" name="user_pass">
 			<br><br>
-			<input type="submit" name="submit" value="Submit">
+			<center><input type="submit" name="submit" value="Submit"></center>
 			</form>
 			<?
 				include 'functions.php';
