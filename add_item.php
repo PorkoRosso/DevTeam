7096b4993504a6c2ec721169a40666504504416c
ABahirat <amba9583@colorado.edu>
2015-12-06 14:04:14 -0700

    Added checkboxes for trade/sell

--- add_item.php
+++ add_item.php
@@ -23,8 +23,11 @@
 			<form method="post" enctype="multipart/form-data" style= "left: 250px; width: 200px;border: 3px solid #101417; background-color: #cfb87b; padding: 25px 70px 25px 70px;"	>
 				Select image to upload:
 				<input type="file" name="fileToUpload" id="fileToUpload">
+				Email: <br><input type ="text" name = "user_email"><br>
 				Item Name: <br><input type ="text" name = "Item_name"><br>
 				Item Price: <br><input type = "text" name = "Item_price"><br>
+				Trade: <br><input type = "checkbox" name = "For_sale"><br>
+				Sale: <br><input type = "checkbox" name = "For_trade"><br>
 				<input type="submit" value="Post Item" name="submit">
 				<?
 					include 'functions.php';
